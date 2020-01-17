0f62ee10277825f7c5958102bafb4c98f8f1d456
Cuong Duy Nguyen <cuongw.me@gmail.com>
2020-01-17 23:41:33 +0700

    Clean source (#3)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,5 @@
 import express, { Request, Response } from 'express';
-import imageRoute from './image.route';
 
 const router = express.Router();
-
-router.use('/image', imageRoute);
 
 export default router;
