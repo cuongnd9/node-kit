27684d4efbce46b67b6169e8a6840aed9f77f95d
Nguyen Duy Cuong <cuongw.me@gmail.com>
2020-02-07 22:19:05 +0700

    Add request component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as checkRole } from './checkRole';
 export { default as handleError } from './handleError';
 export { default as withController } from './withController';
+export { default as request } from './request';
